49e84671b7bc65324aa990c7f493f308d0cf1057
Antonio Molinari <antoniomolinari@me.com>
2026-04-18 17:13:06 +0200

    refactor: simplify layout of Instructions component for improved readability and maintainability

--- src/components/Instructions.vue
+++ src/components/Instructions.vue
@@ -5,33 +5,30 @@ const global = inject('global')
 </script>
 
 <template>
-  <div class="text-center mb-8"> 
-    <p class="text-xl text-gray-500 mb-8">Segui le istruzioni per partecipare!</p>
-    <ul class="list-inside space-y-4 mx-auto px-12">
-      <li class="text-lg flex flex-col items-center gap-4">
-        <div class="font-bold rounded-full text-xl bg-white text-black w-12 h-12 flex items-center justify-center">
-          1
-        </div>
-        Accedi con il tuo account Google
-      </li>
-      <li class="text-lg flex flex-col items-center gap-4">
-        <div class="font-bold rounded-full text-xl bg-white text-black w-12 h-12 flex items-center justify-center">
-          2
-        </div>
-        Scansiona almeno {{ global.settings.qrcodes_required }} QR code che trovi in giro
-      </li>
-      <li class="text-lg flex flex-col items-center gap-4">
-        <div class="font-bold rounded-full text-xl bg-white text-black w-12 h-12 flex items-center justify-center">
-          3
-        </div> 
-        Completa il quiz con le risposte corrette
-      </li>
-      <li class="text-lg flex flex-col items-center gap-4">
-        <div class="font-bold rounded-full text-xl bg-white text-black w-12 h-12 flex items-center justify-center">
-          4
-        </div> 
-        Ricevi il cocktail omaggio
-      </li>
-    </ul>
-  </div>
+  <ul class="text-center mb-8 list-inside space-y-4 mx-auto px-12">
+    <li class="text-lg flex flex-col items-center gap-4">
+      <div class="font-bold rounded-full text-xl bg-white text-black w-12 h-12 flex items-center justify-center">
+        1
+      </div>
+      Accedi con il tuo account Google
+    </li>
+    <li class="text-lg flex flex-col items-center gap-4">
+      <div class="font-bold rounded-full text-xl bg-white text-black w-12 h-12 flex items-center justify-center">
+        2
+      </div>
+      Scansiona almeno {{ global.settings.qrcodes_required }} QR code che trovi in giro
+    </li>
+    <li class="text-lg flex flex-col items-center gap-4">
+      <div class="font-bold rounded-full text-xl bg-white text-black w-12 h-12 flex items-center justify-center">
+        3
+      </div> 
+      Completa il quiz
+    </li>
+    <li class="text-lg flex flex-col items-center gap-4">
+      <div class="font-bold rounded-full text-xl bg-white text-black w-12 h-12 flex items-center justify-center">
+        4
+      </div> 
+      Ricevi il cocktail omaggio
+    </li>
+  </ul>
 </template>
